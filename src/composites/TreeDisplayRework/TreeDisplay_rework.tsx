83b516712b3f52e5befdac2feb11c0eb63b9a1bf
martinhundrup <hundrupm@gmail.com>
2024-05-06 09:53:17 -0700

    fixed error with node links proto

--- src/composites/TreeDisplayRework/TreeDisplay_rework.tsx
+++ src/composites/TreeDisplayRework/TreeDisplay_rework.tsx
@@ -8,15 +8,6 @@ import { LinkHorizontal } from '@visx/shape';
 import { LinkVertical } from '@visx/shape';
 import { LinearGradient } from '@visx/gradient';
 
-const peach = '#fd9b93';
-const pink = '#fe6e9e';
-const blue = '#03c0dc';
-const green = '#26deb0';
-const plum = '#71248e';
-const lightpurple = '#374469';
-const white = '#ffffff';
-export const background = '#272b4d';
-
 // project imports
 import TreeNode from "./TreeNode/TreeNode.jsx";
 import { Color } from 'd3';
@@ -26,162 +17,9 @@ import './TreeDisplay.css';
 const node_width = 120;
 const node_height = 60;
 
-interface TreeNode {
-  name: string;
-  children?: this[];
-}
-
-type HierarchyNode = HierarchyPointNode<TreeNode>;
-
-// tree construction
-const rawTree: TreeNode = {
-  name: 'T',
-  children: [
-    {
-      name: 'A',
-      children: [
-        { name: 'A1' },
-        { name: 'A2' },
-        { name: 'A3' },
-        {
-          name: 'C',
-          children: [
-            {
-              name: 'C1',
-            },
-            {
-              name: 'D',
-              children: [
-                {
-                  name: 'D1',
-                },
-                {
-                  name: 'D2',
-                },
-                {
-                  name: 'D3',
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    },
-    { name: 'Z' },
-    {
-      name: 'B',
-      children: [{ name: 'B1' }, { name: 'B2' }, { name: 'B3' }],
-    },
-  ],
-};
-
-function RootNode({ node }: { node: HierarchyNode }) {
-  return (
-    <Group top={node.x} left={node.y}>
-      <circle r={30} fill="url('#lg')" />
-      <text
-        dy=".33em"
-        fontSize={9}
-        fontFamily="Arial"
-        textAnchor="middle"
-        style={{ pointerEvents: 'none' }}
-        fill={plum}
-      >
-        {node.data.name}
-      </text>
-    </Group>
-  );
-}
-
-function ParentNode({ node }: { node: HierarchyNode }) {
-  const width = node_width;
-  const height = node_height;
-  const centerX = -width / 2;
-  const centerY = -height / 2;
-
-  return (
-    <Group top={node.x} left={node.y}>
-      <rect
-        height={height}
-        width={width}
-        y={centerY}
-        x={centerX}
-        fill={background}
-        stroke={blue}
-        strokeWidth={1}
-        onClick={() => {
-          alert(`clicked: ${JSON.stringify(node.data.name)}`);
-        }}
-      />
-      <text
-        dy=".33em"
-        fontSize={9}
-        fontFamily="Arial"
-        textAnchor="middle"
-        style={{ pointerEvents: 'none' }}
-        fill={white}
-      >
-        {node.data.name}
-      </text>
-    </Group>
-  );
-}
-
-/** Handles rendering Root, Parent, and other Nodes. */
-function Node({ node }: { node: HierarchyNode }) {
-  const width = node_width;
-  const height = node_height;
-  const centerX = -width / 2;
-  const centerY = -height / 2;
-  const isRoot = node.depth === 0;
-  const isParent = !!node.children; // `!!` typecasts the node to a bool
-
-  if (isRoot) return <RootNode node={node} />;
-  if (isParent) return <ParentNode node={node} />;
-
-  return (
-    <Group top={node.x} left={node.y}>
-      <rect
-        height={height}
-        width={width}
-        y={centerY}
-        x={centerX}
-        fill={background}
-        stroke={green}
-        strokeWidth={1}
-        strokeDasharray="2,2"
-        strokeOpacity={0.6}
-        rx={10}
-        onClick={() => {
-          alert(`clicked: ${JSON.stringify(node.data.name)}`);
-        }}
-      />
-      <text
-        dy=".33em"
-        fontSize={9}
-        fontFamily="Arial"
-        textAnchor="middle"
-        fill={green}
-        style={{ pointerEvents: 'none' }}
-      >
-        {node.data.name}
-      </text>
-    </Group>
-  );
-}
-
-const defaultMargin = { top: 10, left: 80, right: 80, bottom: 10 };
-
-export type TreeProps = {
-  width: number;
-  height: number;
-  margin?: { top: number; right: number; bottom: number; left: number };
-};
-
 const min_width = 400; // the min width at which to render the tree
 let width = 900; // width of the background
 let height = 500; // height of the background
-let margin = defaultMargin;
 
 //export default function Example({ width, height, margin = defaultMargin }: TreeProps) { // original
 export function TreeDisplay_Rework(_processedData : any) {
@@ -202,7 +40,6 @@ export function TreeDisplay_Rework(_processedData : any) {
 
   // second row of nodes -- the children of the tqi nodes
   const quality_aspect_nodes : any[] = create_nodes(_processedData.fileData.factors.quality_aspects);
-  console.log(quality_aspect_nodes);
   const quality_aspect_rects : any[] = create_rects(quality_aspect_nodes, 250);
 
   // third row of nodes -- the children of the quality aspect nodes
@@ -211,11 +48,7 @@ export function TreeDisplay_Rework(_processedData : any) {
 
   // draw links between tqi node
   const tqi_links : any[] = draw_links(tqi_rects, quality_aspect_rects);
-
-  // start of example code -- edits have been made
-  const data = useMemo(() => hierarchy(rawTree), []);
-  const yMax = height - margin.top - margin.bottom;
-  const xMax = width - margin.left - margin.right;
+  const qar_links : any[] = draw_links(quality_aspect_rects, product_factor_rects);
 
   // notes:
   // rx is the curvature of the rect
@@ -226,39 +59,16 @@ export function TreeDisplay_Rework(_processedData : any) {
     <svg width={width} height={height}>
       <rect width={width} height={height} rx={10} id='tree_canvas'/>
 
-      {tqi_links}
 
       {tqi_rects}
       {quality_aspect_rects}
       {product_factor_rects}
+
+      
+      {tqi_links}
+      {qar_links}
       
     </svg>
-  
-
-    /*
-    <svg width={width} height={height}>
-      <LinearGradient id="lg" from={peach} to={pink} />
-      <rect width={width} height={height} rx={10} fill={background} />
-      <Tree<TreeNode> root={data} size={[yMax, xMax]}>
-        {(tree) => (
-          <Group top={margin.top} left={margin.left}>
-            {tree.links().map((link, i) => (
-              <LinkHorizontal // draws the path between
-                key={`link-${i}`}
-                data={link}
-                stroke={lightpurple}
-                strokeWidth="1"
-                fill="none"
-              />
-            ))}
-            {tree.descendants().map((node, i) => (
-              <Node key={`node-${i}`} node={node} />
-            ))}
-          </Group>
-        )}
-      </Tree>
-    </svg>
-    */
   );
 } // end of export
 
@@ -285,8 +95,6 @@ function create_nodes(_factors : any){
   const node_y = 15;
 
   _factors = process_data(_factors, 1.0);
-
-  console.log(_factors);
 
   for (let _factor in _factors) {
     nodes.push(
@@ -347,13 +155,23 @@ function create_rects(nodes : any[], _y_pos : number){
 // draws the links from the node to the children nodes
 function draw_links(parents : any[], children : any[]){
 
-let links : any[] = []; // storage for all the lines
+  let links : any[] = []; // storage for all the lines
 
   parents.forEach(function(_parent){
+
+    // get parents x and y coords
+    let parent_x = _parent.props.children[0].props.x + _parent.props.children[0].props.width / 2;
+    let parent_y = _parent.props.children[0].props.y + _parent.props.children[0].props.height;
+
     children.forEach(function(_child){
-      console.log('child ' + _child);
+
+      // get childrens x and y coord
+      let child_x = _child.props.children[0].props.x + _child.props.children[0].props.width / 2;
+      let child_y = _child.props.children[0].props.y;
+
       links.push(
-        <path d={`M${_parent.x} ${_parent.y} L${_child.x} ${_child.y}`} stroke={green} strokeWidth={5} fill={'none'}/>
+        <path d={`M${parent_x} ${parent_y} L${child_x} ${child_y}`} 
+        stroke={'#000000'} strokeWidth={5} fill={'none'}/>
       );
 
     });
