ed816d34a32dd96eb61cededcb3984ce560f7010
martinhundrup <hundrupm@gmail.com>
2024-05-13 15:35:07 -0700

    added up arrow to view edges from one PF node to all QA nodes.

--- src/composites/TreeDisplayRework/TreeDisplay_rework.tsx
+++ src/composites/TreeDisplayRework/TreeDisplay_rework.tsx
@@ -5,22 +5,23 @@ import { CornerTopRightIcon } from '@radix-ui/react-icons';
 
 import React, { useEffect, useMemo, useRef, useState } from 'react';
 import Draggable, { DraggableData, DraggableEvent } from 'react-draggable';
-import {process_data, draw_edges} from './TreeDisplayHelpers.tsx'
+import {process_data, draw_edges, draw_up_edges} from './TreeDisplayHelpers.tsx'
 import { boolean } from "zod";
 import { update } from "ramda";
 import { create } from "d3";
 
+import { EyeOpenIcon, EyeClosedIcon, ArrowUpIcon } from "@radix-ui/react-icons";
 
 // TODO:
 //    - drag nodes                                              ** I need a way to grab the updated x and y coords of the <draggable>
 //    - drag page around
-//    - measures nodes
-//    - diagnostics nodes
+//    - measures nodes                                          ** done **
+//    - diagnostics nodes                                       ** done **
 //    - differentiate between types of nodes
 //    - edge weights                                            ** done ** 
 //    - edit edge weights
 //    - edit node values
-//    - add up arrow and close eye icon to nodes
+//    - add up arrow and close eye icon to nodes                ** FOCUS ** 
 //    - horizontal and vertical scroll bars to tree canvas      ** need to center horizontal bar upon loading ** 
 //    - reset zoom functionality
 //    - reset selection button
@@ -63,13 +64,16 @@ export function TreeDisplay_Rework(_processedData : any) {
   const [active_tqi_node, setActiveTQINode] = useState<any>(null);
   const [active_quality_aspect_node, setActiveQualityAspectNode] = useState<any>(null);
   const [active_product_factor_node, setActiveProductFactorNode] = useState<any>(null);
+  const [upwards_product_factor_node, setUpwardsProductFactorNode] = useState<any>(null);
   const [active_measure_node, setActiveMeasureNode] = useState<any>(null);
 
   const [tqi_edges, setTQIEdges] = useState<any[]>([]);
   const [qa_edges, setQAEdges] = useState<any[]>([]);
   const [pf_edges, setPFEdges] = useState<any[]>([]);
   const [measure_edges, setMeasureEdges] = useState<any[]>([]);
+  const [PF_up_edges, setPFUpEdges] = useState<any[]>([]);
   const [node_clicked_marker, setNodeClickedMarker] = useState<MouseEvent>();
+  const [arrow_clicked_marker, setArrowClickedMarker] = useState<MouseEvent>();
 
   // called when a button is pressed.
   // used to ensure the button clicks update with the newest information
@@ -87,6 +91,9 @@ export function TreeDisplay_Rework(_processedData : any) {
     const clickedMeasure = measure_nodes.find((_node) => _node.name === newNodeId);
     
     if (clickedTQI) {
+      //console.log(clickedTQI);
+      console.log(clickedTQI._rect.props.children.props.children[3]);
+      clickedTQI._rect.props.children.props.children[3];
       // case: it is not active, so the active node must be switched to it
       if (!active_tqi_node || clickedTQI.name !== active_tqi_node.name) { 
         setActiveTQINode(clickedTQI);
@@ -127,6 +134,35 @@ export function TreeDisplay_Rework(_processedData : any) {
     }
 
   }, [node_clicked_marker]); // Empty dependency array ensures it runs only once after component mount
+  
+  // calls the above use effect when a button is pressed.
+  function node_clicked(e : MouseEvent){
+    setNodeClickedMarker(e);
+  }
+
+  // calls the arrow clicked use effect func.
+  function arrow_clicked(e : any){
+    setArrowClickedMarker(e);
+  }
+
+  // called when an up arrow is pressed on one of the PF nodes.
+  useEffect(() => {
+
+    if (!arrow_clicked_marker) // so it doesn't break on initial load
+      return;
+
+    // .substring(8) cleanes the 'uparrow ' part of id
+    const clickedPF = product_factor_nodes.find((_node) => _node.name === arrow_clicked_marker.target.id.substring(8));
+
+    if (upwards_product_factor_node && clickedPF.name === upwards_product_factor_node.json_data.name){
+      setPFUpEdges([]); // erase the lines if the active node was pressed
+      setUpwardsProductFactorNode(null);
+      return;
+    }
+
+    setUpwardsProductFactorNode(clickedPF);
+    setPFUpEdges(draw_up_edges(clickedPF, quality_aspect_nodes));
+  }, [arrow_clicked_marker]);
 
   // called when a product factor node is pressed to display the appropriate measure nodes in the correct location
   useEffect(() => {
@@ -146,7 +182,7 @@ export function TreeDisplay_Rework(_processedData : any) {
       }
     }
 
-    setMeasureNodes(create_nodes(new_measures, active_product_factor_node._x + 75, 650));
+    setMeasureNodes(create_nodes(new_measures, active_product_factor_node._x + 75, 650, false));
 
   }, [active_product_factor_node]);
 
@@ -167,15 +203,9 @@ export function TreeDisplay_Rework(_processedData : any) {
       }
     }
 
-    setDiagnosticNodes(create_nodes(new_diagnostics, active_measure_node._x + 75, 800));
+    setDiagnosticNodes(create_nodes(new_diagnostics, active_measure_node._x + 75, 800, false));
 
   }, [active_measure_node]);
-  
-  // calls the above use effect when a button is pressed.
-  function node_clicked(e : MouseEvent){
-
-    setNodeClickedMarker(e);
-  }
 
   // used to display the updated node lists
   useEffect(() => {
@@ -183,13 +213,13 @@ export function TreeDisplay_Rework(_processedData : any) {
     function set_nodes() {
 
       // top row of nodes -- function similar to root nodes (only 1 with doctored data file)
-      setTQINodes(create_nodes(_processedData.fileData.factors.tqi, width / 2, 100)); // holds the data of each node
+      setTQINodes(create_nodes(_processedData.fileData.factors.tqi, width / 2, 100, false)); // holds the data of each node
 
       // second row of nodes -- the children of the tqi nodes
-      setQualityAspectNodes(create_nodes(_processedData.fileData.factors.quality_aspects, width / 2, 250));
+      setQualityAspectNodes(create_nodes(_processedData.fileData.factors.quality_aspects, width / 2, 250, false));
 
       // third row of nodes -- the children of the quality aspect nodes
-      setProductFactorNodes(create_nodes(_processedData.fileData.factors.product_factors, width / 2, 500));
+      setProductFactorNodes(create_nodes(_processedData.fileData.factors.product_factors, width / 2, 500, true));
     }
 
     set_nodes();
@@ -231,6 +261,7 @@ export function TreeDisplay_Rework(_processedData : any) {
         {tqi_edges}
         {qa_edges}
         {pf_edges}
+        {PF_up_edges}
         {measure_edges}
 
       </svg>
@@ -238,7 +269,7 @@ export function TreeDisplay_Rework(_processedData : any) {
   );
 
   // creates and returns an array of nodes representing the specified layer
-  function create_nodes(_factors : any, _x_pos: number, _y_pos: number){
+  function create_nodes(_factors : any, _x_pos: number, _y_pos: number, _arrow : boolean){
 
     // ** REQUIRED TO PROCESS DATA ** //
     // TODO: will need to refactor so it doesn't break without
@@ -273,24 +304,31 @@ export function TreeDisplay_Rework(_processedData : any) {
         _factors[index],
         (<Draggable key={_factors[index].name} disabled={true}
         onMouseDown={node_clicked}
-        >
+        >   
             <g>
+            
               <rect height={node_height} width={node_width} x={x} y={y} className={node_id} id={_factors[index].name}/>
-              <text x={x + node_width / 2} y={y + node_height / 4} className={'node_text'}>
+              <text x={x + node_width / 2} y={10+y + node_height / 4} className={'node_text'}>
                 {_factors[index].name}
               </text>
-              <text x={x + node_width / 2} y={y + node_height / 2} className={'node_text'}>
+              <text x={x + node_width / 2} y={10+y + node_height / 2} className={'node_text'}>
                 {_factors[index].value.toFixed(2)}
               </text>
+
+              {_arrow ? <>
+              <rect id={'uparrow ' + _factors[index].name} onClick={(e) => arrow_clicked(e)} height={20} width={25} x={x} y={y} className={node_id}/>
+              <ArrowUpIcon x={x + 5} y={y+2}/> </> : <></>}
+
+              <rect onClick={() => {console.log('eye icon');}} height={20} width={25} x={x + node_width - 25} y={y} className={node_id}/>
+              <EyeOpenIcon onClick={() => {console.log('eye icon');}}  x={x + node_width - 20} y={y + 2}/>
             </g>
           </Draggable>),
         node_width,
         node_height,
         x,
         y
-      )
+      );
     });
   }
 
 } // end of export
-
