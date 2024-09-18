2b0f59fb51f96a7c5ee88c3791433cb92ea74810
martinhundrup <hundrupm@gmail.com>
2024-09-18 12:14:04 -0700

    Implemented IBM cb palette for main page.

--- src/composites/LegendContainer/Legend.tsx
+++ src/composites/LegendContainer/Legend.tsx
@@ -9,11 +9,11 @@ export const LegendContainer = () => {
 
     <Flex gap="2" align='center'>
       <Text weight="bold" as="div" color="gray" size="5"> <Strong>Risk Level   </Strong></Text>
-      <Badge color="red" size="2">Severe</Badge>
-      <Badge color="orange" size="2">High</Badge>
-      <Badge color="yellow" size="2">Medium</Badge>
-      <Badge color="blue" size="2">Low</Badge>
-      <Badge color="green" size="2">Insignificant</Badge>
+      <Badge style={{ backgroundColor: "#DC267F" }} size="2">Severe</Badge>
+      <Badge style={{ backgroundColor: "#FE6100" }} size="2">High</Badge>
+      <Badge style={{ backgroundColor: "#FFB000" }} size="2">Medium</Badge>
+      <Badge style={{ backgroundColor: "#785EF0" }} size="2">Low</Badge>
+      <Badge style={{ backgroundColor: "#648FFF" }} size="2">Insignificant</Badge>
     </Flex>
 
   );
--- src/composites/Overview/PieChartColor.ts
+++ src/composites/Overview/PieChartColor.ts
@@ -1,11 +1,11 @@
 
 // Define colors for each slice of the pie chart
 export const COLORS: { [key: string]: string } = {
-  Severe: "#f3000d80",
-  High: "#ff9c0080",
-  Medium: "#ffee0080",
-  Moderate: "#ffee0080",
-  Minor: "#008ff580",
-  Low: "#008ff580",
-  Insignificant: "#00a43380",
+  Severe: "#DC267F",
+  High: "#FE6100",
+  Medium: "#FE6100",
+  Moderate: "#FFB000",
+  Minor: "#785EF0",
+  Low: "#785EF0",
+  Insignificant: "#648FFF",
 };
